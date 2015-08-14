5ebd4edc9e76105a912c38eb5e9104f7695d0de2
Santiago Palladino <spalladino@gmail.com>
2015-08-14 17:03:06 -0300

    Added ruby 1.9

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ build() {
   rm -rf ruby-$1
   mkdir ruby-$1
   cp -r files ruby-$1/files
-  RUBY_VERSION=$1 erb Dockerfile.erb > ruby-$1/Dockerfile
+  FROM_IMAGE=$2 erb Dockerfile.erb > ruby-$1/Dockerfile
 }
 
-
-build 2.0
-build 2.1
-build 2.2
+build 1.9 instedd/ruby:1.9
+build 2.0 ruby:2.0
+build 2.1 ruby:2.1
+build 2.2 ruby:2.2
