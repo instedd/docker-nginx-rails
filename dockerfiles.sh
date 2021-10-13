938dba5982dfb7e90de3597af45cd65763dfb9cf
Matias Garcia Isaia <mgarcia@manas.tech>
2021-10-13 20:49:58 -0300

    Cleanup expired Let's Encrypt CA
    Let's Encrypt old Certificate Authority expired on Sept 30, 2021.
    
    Most of InSTEDD's infra uses those certificates, so this patch
    makes new certificates work again.

--- dockerfiles.sh
+++ dockerfiles.sh
@@ -4,10 +4,10 @@ dockerfile-for() {
   rm -rf ruby-$1
   mkdir ruby-$1
   cp -r files ruby-$1/files
-  FROM_IMAGE=$2 erb Dockerfile.erb > ruby-$1/Dockerfile
+  FROM_IMAGE=$2 RUBYGEMS_VERSION=$3 erb Dockerfile.erb > ruby-$1/Dockerfile
 }
 
-dockerfile-for 1.9 instedd/ruby:1.9
+dockerfile-for 1.9 instedd/ruby:1.9 1.8.30
 dockerfile-for 2.0 ruby:2.0
 dockerfile-for 2.1 ruby:2.1
 dockerfile-for 2.2 ruby:2.2
